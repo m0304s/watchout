a767bd106a967aabc019d53f9607eb0195b3a8a8
최민석 <gd10080008@gmail.com>
2025-09-08 22:24:41 +0900

    Develop

--- frontend-repo/src/App.tsx
+++ frontend-repo/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more11
+        Click on the Vite and React logos to learn more-cicd-test
       </p>
     </>
   )
