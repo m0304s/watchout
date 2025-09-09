3ea3398b8407102aeed9ad8ddda0f337b78d1cf4
yena <yeneua.dev@gmail.com>
2025-09-10 01:47:25 +0900

    [FE] chore: @ alias 절대 경로 설정 추가

--- frontend-repo/vite.config.ts
+++ frontend-repo/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import babel from 'vite-plugin-babel'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +13,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
