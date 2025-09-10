b4bd7c30b602eb37d019cdc02f0c76bedc4fa907
yena <yeneua.dev@gmail.com>
2025-09-10 23:33:42 +0900

    [FE] chore: capacitor 설정
    - ts 타입 체크 명령어, 빌드 명령어 분리(package.json 파일)
    - 기존의 build: tsc -b && vite build 빌드 명령어를 typecheck: tsc -b, build: vite build로 분리

---
+++ frontend-repo/android/app/src/main/java/com/ssafy/watchout/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ssafy.watchout;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend-repo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.ssafy.watchout',
+  appName: 'watch-out',
+  webDir: 'dist',
+}
+
+export default config
