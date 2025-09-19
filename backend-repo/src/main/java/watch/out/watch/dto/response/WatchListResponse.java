ef4aba0c9acbb87f0ec3f0b56674b7072ff3ab5d
박정훈 <qwg531@naver.com>
2025-09-19 12:24:10 +0900

    [BE] fix: 워치 목록 response에서 유저ID에서 유저이름으로 수정
    * [BE] fix: 워치 목록 response에서 유저ID에서 유저이름으로 수정

--- backend-repo/src/main/java/watch/out/watch/dto/response/WatchListResponse.java
+++ backend-repo/src/main/java/watch/out/watch/dto/response/WatchListResponse.java
@@ -11,20 +11,20 @@ public record WatchListResponse(
     Integer watchId,
     String modelName,
     WatchStatus status,
-    LocalDateTime rentedAt, // 마지막 대여 일시
+    LocalDateTime rentedAt,
     String note,
-    String userId // 현재 대여 중인 사용자 사번
+    String userName
 ) {
 
     @QueryProjection
     public WatchListResponse(UUID watchUuid, Integer watchId, String modelName, WatchStatus status,
-        LocalDateTime rentedAt, String note, String userId) {
+        LocalDateTime rentedAt, String note, String userName) {
         this.watchUuid = watchUuid;
         this.watchId = watchId;
         this.modelName = modelName;
         this.status = status;
         this.rentedAt = rentedAt;
         this.note = note;
-        this.userId = userId;
+        this.userName = userName;
     }
 }
